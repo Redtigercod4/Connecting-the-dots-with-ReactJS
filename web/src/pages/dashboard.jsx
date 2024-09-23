85598ca87710eefcc80b3d6f362bdee095ee87d1
Wes <76014409+Redtigercod4@users.noreply.github.com>
2024-09-23 20:05:59 +0000

    Data Fetching

--- web/src/pages/dashboard.jsx
+++ web/src/pages/dashboard.jsx
@@ -6,24 +6,30 @@ import React from "react";
 function Dashboard() {
     const [data, setData] = React.useState();
 
+    React.useEffect(() => {
+        fetch("https://api.aetherion.workers.dev/")
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }, [])
+
     return (
-       <Layout>
-        <TableContainer>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Tail Number</TableCell>
-                        <TableCell>Status</TableCell>
-                        <TableCell>Previous Location</TableCell>
-                        <TableCell>Next Location</TableCell>
-                    </TableRow>
-                </TableHead>
-            </Table>
-            <TableBody>
-                <Tile props={data} />
-            </TableBody>
-        </TableContainer>
-       </Layout>
+        <Layout>
+            <TableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Tail Number</TableCell>
+                            <TableCell>Status</TableCell>
+                            <TableCell>Previous Location</TableCell>
+                            <TableCell>Next Location</TableCell>
+                        </TableRow>
+                    </TableHead>
+                </Table>
+                <TableBody>
+                    <Tile props={data} />
+                </TableBody>
+            </TableContainer>
+        </Layout>
     )
 }
 
